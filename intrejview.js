c45ff95f4f43d888b28c4d3f28ae1c88b6dc3d8a
HumayunKabir1981 <callmilton.com>
2026-02-24 01:41:26 +0600

    interview to reject

--- intrejview.js
+++ intrejview.js
@@ -6,7 +6,7 @@ const rejectedCountDisplay = document.getElementById('rejectedJob');
 
 
 function updateCounters() {
-    
+
     const intCount = interviewSection.querySelectorAll('.js-jobCard').length;
     interviewCountDisplay.innerText = intCount;
 
@@ -20,14 +20,14 @@ const interviewSection = document.getElementById('interview-Section');
 const btnInterviewSections = document.getElementsByClassName("js-add-interview");
 for (let btn of btnInterviewSections) {
     btn.addEventListener("click", function (event) {
-       
-        
+
+
         const jobCard = event.target.closest('.js-jobCard');
         const ribon = jobCard.querySelector('.ribon');
         ribon.innerHTML = `<button class="btn btn-outline btn-accent ">INTERVIEW</button>`;
 
         const jobCardClone = jobCard.cloneNode(true);
-       
+
         interviewSection.appendChild(jobCardClone);
         updateCounters();
     });
@@ -39,12 +39,12 @@ const btnRejectSections = document.getElementsByClassName("js-add-rejected");
 for (let btn of btnRejectSections) {
     btn.addEventListener("click", function (event) {
         const jobCard = event.target.closest('.js-jobCard');
-         const ribon = jobCard.querySelector('.ribon');
+        const ribon = jobCard.querySelector('.ribon');
         ribon.innerHTML = `<button class="btn btn-outline btn-secondary ">REJECTED</button>`;
 
         const jobCardClone = jobCard.cloneNode(true);
 
-      
+
         rejectSection.appendChild(jobCardClone);
         updateCounters();
     });
@@ -55,19 +55,46 @@ document.addEventListener('click', function (event) {
 
 
     if (event.target.closest('.js-btn-remove')) {
-        const card = event.target.closest('.bg-white');
+        const card = event.target.closest('.js-jobCard');
         card.remove();
         updateCounters();
     }
 
- 
-    if (event.target.classList.contains('js-add-rejected') || event.target.innerText === 'REJECTED') {
-        const card = event.target.closest('.bg-white');
 
-      
-        if (interviewSection.contains(card)) {
-            rejectedSection.appendChild(card); 
+    if (event.target.classList.contains('js-add-rejected') || event.target.innerText === 'REJECTED') {
+        const card = event.target.closest('.js-jobCard');
+
+        if (rejectedSection.contains(card)) {
+            interviewSection.appendChild(card);
             updateCounters();
         }
+
+
+        if (interviewSection.contains(card)) {
+            rejectedSection.appendChild(card);
+            updateCounters();
+        }
+
     }
+
+
+    if (event.target.classList.contains('js-add-interview') || event.target.innerText === 'INTERVIEW') {
+        const card = event.target.closest('.js-jobCard');
+
+      
+
+
+        if (interviewSection.contains(card)) {
+            rejectedSection.appendChild(card);
+            updateCounters();
+        }
+
+          if (rejectedSection.contains(card)) {
+            interviewSection.appendChild(card);
+            updateCounters();
+        }
+
+
+    }
+
 });
